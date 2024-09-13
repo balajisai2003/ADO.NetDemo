72091bae654dd8e4453b57940f2210a32227bbb5
BalajiSai2003 <balajisai2003@outlook.com>
2024-09-13 11:39:42 +0530

    Add Form4 with sync/async DB operations; update startup form
    A new form, Form4, has been added to the project. This includes:
    - Updated project file to include Form4.resx as an embedded resource.
    - Created Form4.Designer.cs to define layout and components.
    - Created Form4.cs to handle logic for sync/async DB operations.
    - Added Form4.resx for resource definitions.
    
    Updated Program.cs to set Form4 as the startup form instead of Form3.

---
+++ AOD.NetDemo/Form4.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace AOD.NetDemo
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSyncExe = new System.Windows.Forms.Button();
+            this.btnAsyncExe = new System.Windows.Forms.Button();
+            this.btnAsyncWait = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSyncExe
+            // 
+            this.btnSyncExe.Location = new System.Drawing.Point(74, 51);
+            this.btnSyncExe.Name = "btnSyncExe";
+            this.btnSyncExe.Size = new System.Drawing.Size(75, 23);
+            this.btnSyncExe.TabIndex = 0;
+            this.btnSyncExe.Text = "Sync Exe";
+            this.btnSyncExe.UseVisualStyleBackColor = true;
+            this.btnSyncExe.Click += new System.EventHandler(this.btnSyncExe_Click);
+            // 
+            // btnAsyncExe
+            // 
+            this.btnAsyncExe.Location = new System.Drawing.Point(74, 122);
+            this.btnAsyncExe.Name = "btnAsyncExe";
+            this.btnAsyncExe.Size = new System.Drawing.Size(75, 23);
+            this.btnAsyncExe.TabIndex = 1;
+            this.btnAsyncExe.Text = "Async Exe";
+            this.btnAsyncExe.UseVisualStyleBackColor = true;
+            this.btnAsyncExe.Click += new System.EventHandler(this.btnAsyncExe_Click);
+            // 
+            // btnAsyncWait
+            // 
+            this.btnAsyncWait.Location = new System.Drawing.Point(74, 169);
+            this.btnAsyncWait.Name = "btnAsyncWait";
+            this.btnAsyncWait.Size = new System.Drawing.Size(75, 23);
+            this.btnAsyncWait.TabIndex = 2;
+            this.btnAsyncWait.Text = "Async Methods";
+            this.btnAsyncWait.UseVisualStyleBackColor = true;
+            this.btnAsyncWait.Click += new System.EventHandler(this.btnAsyncWait_Click);
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAsyncWait);
+            this.Controls.Add(this.btnAsyncExe);
+            this.Controls.Add(this.btnSyncExe);
+            this.Name = "Form4";
+            this.Text = "Form4";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSyncExe;
+        private System.Windows.Forms.Button btnAsyncExe;
+        private System.Windows.Forms.Button btnAsyncWait;
+    }
+}
---
+++ AOD.NetDemo/Form4.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AOD.NetDemo
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSyncExe_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(Helper.ConnStr);
+            SqlCommand cmd = new SqlCommand("GetAllEmps",conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            string s = "";
+            while (reader.Read())
+            {
+                s += reader[0]+"\n";
+            }
+            reader.Close();
+            conn.Close();
+            MessageBox.Show(s); 
+        }
+
+        private void btnAsyncExe_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection conn = new SqlConnection(Helper.ConnStr);
+            SqlCommand cmd = new SqlCommand("GetAllEmps", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            conn.Open();
+            //SqlDataReader reader = cmd.ExecuteReader();
+            IAsyncResult ar = cmd.BeginExecuteNonQuery();
+            MessageBox.Show("Contionue.........");
+            while (!ar.IsCompleted)
+            {
+                Application.DoEvents();
+                System.Threading.Thread.Sleep(200);
+            }
+            SqlDataReader reader = cmd.EndExecuteReader(ar);
+            string s = "";
+            while (reader.Read())
+            {
+                s += reader[0] + "\n";
+            }
+            reader.Close();
+            conn.Close();
+            MessageBox.Show(s);
+        }
+
+        private async void btnAsyncWait_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(Helper.ConnStr);
+            SqlCommand cmd = new SqlCommand("GetAllEmps", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            conn.Open();
+            //SqlDataReader reader = cmd.ExecuteReader();
+            //Task<SqlDataReader> task = cmd.ExecuteReaderAsync();
+            //SqlDataReader reader = await task;
+            SqlDataReader reader = await cmd.ExecuteReaderAsync();
+            string s = "";
+            while (reader.Read())
+            {
+                s += reader[0] + "\n";
+            }
+            reader.Close();
+            conn.Close();
+            MessageBox.Show(s);
+        }
+    }
+}
